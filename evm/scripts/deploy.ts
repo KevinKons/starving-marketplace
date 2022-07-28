2ddb24050856b5b97c4429f6dd2a556b24d99fd5
kevin kons <kevin.kons@hotmail.com>
2022-07-27 23:57:32 -0300

    Removing greeter

--- evm/scripts/deploy.ts
+++ evm/scripts/deploy.ts
@@ -1,12 +1,6 @@
 import { ethers } from 'hardhat';
-import { Greeter } from '../typechain-types';
 
 async function main() {
-    const Greeter = await ethers.getContractFactory('Greeter');
-    const greeter: Greeter = await Greeter.deploy();
-    console.log('Deploying...');
-    await greeter.deployed();
-    console.log('Contract deployed at:', greeter.address);
 }
 
 main().catch((error) => {
