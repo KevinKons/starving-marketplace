43d066b09c17c03b337a6bc0d85d1f7d3947e8eb
kevin kons <kevin.kons@hotmail.com>
2022-07-28 00:38:21 -0300

    Implementing getAllNftsByOwner

--- evm/contracts/SideAStarvingNFT.sol
+++ evm/contracts/SideAStarvingNFT.sol
@@ -9,18 +9,18 @@ import "./SideBStarvingNFT.sol";
 contract SideAStarvingNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
-    event Mint(address to, uint tokenId);
-    
+    event Mint(address to, uint256 tokenId);
+
     Counters.Counter private _tokenIds;
     SideBStarvingNFT public sideB;
 
     constructor() ERC721("Starving Children Side A", "STA") {}
 
-    function mint(address to, string memory tokenURISideA, string calldata tokenURISideB)
-        public
-        onlyOwner
-        returns (uint256)
-    {
+    function mint(
+        address to,
+        string memory tokenURISideA,
+        string calldata tokenURISideB
+    ) public onlyOwner returns (uint256) {
         uint256 newItemId = _tokenIds.current();
         _mint(to, newItemId);
         _setTokenURI(newItemId, tokenURISideA);
@@ -32,10 +32,22 @@ contract SideAStarvingNFT is ERC721URIStorage, Ownable {
         return newItemId;
     }
 
-    function setSideB(SideBStarvingNFT _sideB) 
-        external 
-        onlyOwner
+    function getAllNftsByOwner(address _owner)
+        external
+        view
+        returns (uint256[] memory ids, string[] memory uris)
     {
+        uint nextPostion;
+        for (uint256 i; i < _tokenIds.current(); i++) {
+            if (ownerOf(i) == _owner) {
+                ids[nextPostion] = i;
+                uris[nextPostion] = tokenURI(i);
+                nextPostion++;
+            } 
+        }
+    }
+
+    function setSideB(SideBStarvingNFT _sideB) external onlyOwner {
         sideB = _sideB;
     }
 }
