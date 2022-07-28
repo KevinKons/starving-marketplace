56abafd439a1edec543e48ae2ff279f11a36e5ec
kevin kons <kevin.kons@hotmail.com>
2022-07-27 22:09:39 -0300

    adding nfts

---
+++ evm/contracts/SideAStarvingNFT.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./SideBStarvingNFT.sol";
+
+contract SideAStarvingNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    
+    Counters.Counter private _tokenIds;
+    SideBStarvingNFT sideB;
+
+    constructor() ERC721("Starving Children Side A", "STA") {}
+
+    function mint(address to, string memory tokenURISideA, string calldata tokenURISideB)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        uint256 newItemId = _tokenIds.current();
+        _mint(to, newItemId);
+        _setTokenURI(newItemId, tokenURISideA);
+
+        sideB.mint(to, tokenURISideB);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+
+    function setSideB(SideBStarvingNFT _sideB) 
+        external 
+        onlyOwner
+    {
+        sideB = _sideB;
+    }
+}
---
+++ evm/contracts/SideBStarvingNFT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SideBStarvingNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    address sideA;
+
+    constructor(address _sideA) ERC721("Starving Children Side B", "STB") {
+        sideA = _sideA;
+    }
+
+    function mint(address to, string memory tokenURI) public {
+        require(_msgSender() == sideA, "Sender is not side A");
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(to, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+    }
+}
