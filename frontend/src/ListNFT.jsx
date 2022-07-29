e687e474de6c742f430f2ddf831fa35fa4b42a20
kevin kons <kevin.kons@hotmail.com>
2022-07-29 12:09:33 -0300

    Removing unused code

--- frontend/src/ListNFT.jsx
+++ frontend/src/ListNFT.jsx
@@ -2,20 +2,7 @@ import Nullstack from 'nullstack';
 import CardNFT from './CardNFT';
 
 class ListNFT extends Nullstack {
-
-  prepare({ project, page }) {
-  }
-
-  renderLink({ children, href }) {
-    const link = href + '?ref=create-nullstack-app';
-    return (
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        {children}
-      </a>
-    )
-  }
-
-
+  
   render({ project }) {
     return (
       <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
