862c20e55210eb60fa9873ebf64d9419fe6e8bf7
kevin kons <kevin.kons@hotmail.com>
2022-08-01 14:22:00 -0300

    Removing rounded border from nft card

--- frontend/src/CardNFT.jsx
+++ frontend/src/CardNFT.jsx
@@ -1,7 +1,7 @@
 export default function CardNFT({ nft }) {
   return (
     <div class="group relative bg-black border-2 border-white p-2">
-      <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+      <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
         <img src={nft.image} class="w-full h-full object-center object-cover lg:w-full lg:h-full" />
       </div>
       <div class="mt-4 flex flex-col justify-between text-white">
