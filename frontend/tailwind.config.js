59ed08de8c236b51a6dcffc199854649d57ff337
kevin kons <kevin.kons@hotmail.com>
2022-08-01 17:03:49 -0300

    Adding new spaces to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '368': '368px',
+        '613': '613px',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio')
