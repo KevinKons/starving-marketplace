0112eea4cac81e29158d08cde5c1574d5259fe59
kevin kons <kevin.kons@hotmail.com>
2022-08-01 17:09:56 -0300

    Taps skeleton

--- frontend/src/TapsPage.jsx
+++ frontend/src/TapsPage.jsx
@@ -4,8 +4,13 @@ class TapsPage extends Nullstack {
 
   render() {
     return (
-      <div>
-        Taps Page
+      <div class='w-full flex flex-col items-center justify-center text-white '>
+        <div class='text-3xl font-bold m-16'>
+          Buy TAPs
+        </div>
+        <div class='h-368 w-613 bg-black border border-1 border-gray-600'>
+          
+        </div>
       </div>
     )
   }
