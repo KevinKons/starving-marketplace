bdf39093741223ec395089b629dece75e41b1441
kevin kons <kevin.kons@hotmail.com>
2022-08-01 23:42:29 -0300

    Adding test implementation

--- evm/test/Taps.test.ts
+++ evm/test/Taps.test.ts
@@ -1,9 +1,8 @@
 import { expect } from 'chai';
-import { ethers } from 'hardhat';
+import { ethers, waffle } from 'hardhat';
 import { Tap } from '../typechain-types';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { BigNumber } from 'ethers';
-
 
 describe('Test NFTs', () => {
     let owner: SignerWithAddress;
@@ -20,5 +19,15 @@ describe('Test NFTs', () => {
     it('Name and Symbol shoud be correct', async () => {
         expect(await tapToken.name()).to.equal('TAP');
         expect(await tapToken.symbol()).to.equal('TAP');
+    });
+
+    it('When buying should send the correct amout of taps token to buyer', async () => {
+        await expect(tapToken.buy({ value: BigNumber.from('2') }))
+            .to.emit(tapToken, 'Buy')
+            .withArgs(owner.address, BigNumber.from('1'));
+        expect(await tapToken.balanceOf(owner.address)).to.equal(BigNumber.from('1'));
+
+        const balanceResult = await waffle.provider.getBalance(tapToken.address);
+        expect(balanceResult).to.equal(BigNumber.from('2'));
     });
 });
