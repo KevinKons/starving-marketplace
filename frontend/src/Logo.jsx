e2745e2214929a6e8ec8069387d1457245891b42
kevin kons <kevin.kons@hotmail.com>
2022-08-03 18:23:59 -0300

    Refactoring AdminLogo component

--- frontend/src/Logo.jsx
+++ frontend/src/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo() {
     return (
-        <div>
-            <div class='logo1'>NFTS FOR</div>
+        <div class='text-white'>
+            <div>NFTS FOR</div>
             <div>STARVING</div>
             <div>CHILDREN</div>
         </div>
