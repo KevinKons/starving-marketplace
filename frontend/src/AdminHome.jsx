8c86cd2ff96f358909881eae06e96dc2f6bce12e
kevin kons <kevin.kons@hotmail.com>
2022-08-03 20:57:10 -0300

    Adding quantity input and mint & send button on AdminHome component

--- frontend/src/AdminHome.jsx
+++ frontend/src/AdminHome.jsx
@@ -19,17 +19,23 @@ class AdminHome extends Nullstack {
             <label>Address</label>
             <input bind={this.address} value={this.address ? this.address : undefined} placeholder='Enter your Wallet address (0x...) or ENS Domain' class="mt-1 block w-full sm:text-sm bg-gray-900 border border-white h-10" />
           </div>
-          <div class='flex justify-between text-xl font-semibold mt-10'>
+          <div class='flex justify-between text-xl font-semibold mt-5'>
             <div>
-              <label>Address</label>
+              <label>Quantity</label>
               <p class='text-gray-400 font-normal text-sm'>
                 Put the quantity of TAPs you'll need
               </p>
             </div>
-            <div>
-              <input bind={this.quantity} value={this.quantity ? this.quantity : undefined}  class="mt-1 block w-10 sm:text-sm bg-gray-900 border border-white h-10" />
+            <div class='flex items-center'>
+              <div class='border border-white h-10 flex items-center'>
+                <img class='p-2' src="tap-symbol.png" />
+              </div>
+              <input bind={this.quantity} value={this.quantity ? this.quantity : undefined} class='text-xl h-10 px-3 block w-20 bg-gray-900 border border-white' placeholder='0.00' />
             </div>
           </div>
+          <button onclick={this.onCreateNFT} class="h-12 w-32 mt-10 py-2 px-4 font-bold text-black bg-red-400 hover:bg-red-500">
+            Mint & Send
+          </button>
         </div>
       </div>
     )
