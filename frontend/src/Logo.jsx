27efbc6da3c95d7359f42e167f6620bb2478b5dc
kevin kons <kevin.kons@hotmail.com>
2022-08-03 16:52:13 -0300

    Creating logo component

---
+++ frontend/src/Logo.jsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+    return (
+        <div>
+            <div class='logo1'>NFTS FOR</div>
+            <div>STARVING</div>
+            <div>CHILDREN</div>
+        </div>
+    )
+}
