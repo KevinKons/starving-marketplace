e9bc1fbf7df2f1091c4124291e5e5e155cef19ac
kevin kons <kevin.kons@hotmail.com>
2022-08-03 16:55:30 -0300

    Refactoring NAvbar to use Logo component

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,4 +1,5 @@
 import Nullstack from 'nullstack';
+import Logo from './Logo'
 
 class Navbar extends Nullstack {
   render() {
@@ -7,11 +8,7 @@ class Navbar extends Nullstack {
         <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           <div class="relative flex items-center justify-between h-16">
             <div class="flex-1 flex justify-between items-center">
-              <div>
-                <div class='logo1'>NFTS FOR</div>
-                <div>STARVING</div>
-                <div>CHILDREN</div>
-              </div>
+              <Logo />
               <div class="hidden sm:block sm:ml-6">
                 <div class="flex space-x-4">
                   <a href="/home" class="px-3 py-2 text-sm font-semibold">Home</a>
