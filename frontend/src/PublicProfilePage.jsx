8ba2aaa7f52aa1e28658ec43832873f587dbabea
kevin kons <kevin.kons@hotmail.com>
2022-08-04 14:52:49 -0300

    Refactoring tap symbol on PublicProfilePage.jsx

--- frontend/src/PublicProfilePage.jsx
+++ frontend/src/PublicProfilePage.jsx
@@ -25,8 +25,8 @@ class PublicProfilePage extends Nullstack {
     return (
       <section class='flex flex-col items-start text-black mt-28'>
         <div class='flex space-x-5 items-center'>
-          <div class='bg-yellow-300 w-24 h-24 ml-1'>
-            <img class='h-6 mr-4' src="tap-symbol.png" />
+          <div class='bg-yellow-300 w-24 h-24 ml-1 flex justify-center items-center grow'>
+            <img class='h-8' src="tap-symbol.png" />
           </div>
           <div class='border border-1 px-3 text-gray-400 text-sm'>
             {`${params.address.slice(0, 14)}....${params.address.slice(params.address.length - 7)}`}
